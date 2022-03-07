5371c2ab3d8c1b0a776062605d54cbd20062741a
yuyamizorogi <uya116@gmail.com>
2022-03-07 12:47:38 +0900

    Submittedコンポーネント一旦完成

--- components/Submitted.vue
+++ components/Submitted.vue
@@ -1,22 +1,23 @@
 <template>
   <div>
-  <v-list three-line>
-    <template v-for="(comment, index) in submitted_cards">
-      <v-list-item
-          :key="index"
-      >
-      <v-avatar
-        color="teal"
-        size="56"
-      ></v-avatar>
-      </v-list-item>
-    </template>
-  </v-list>
+    <v-row>
+      <v-col>
+        <v-text-field readonly value="Submitted Cards"></v-text-field>
+      </v-col>
+    </v-row>
+    <!-- そのうち複数 row で出すようにする -->
+    <!-- 参考: https://qiita.com/taki_tflare/items/015fd98c83c5e5ac511e -->
+    <v-row v-for="(row, key) in 1" :key="key">
+      <div v-for="(comment, key2) in submitted_cards" :key="key2">
+        <v-col><v-avatar color="teal" size="56">{{comment}}</v-avatar></v-col>
+        <v-divider/>
+      </div>
+    </v-row>
   </div>
 </template>
 
 <script>
-import {query, collection, where, getDocs} from "firebase/firestore";
+import {query, collection, where, orderBy, getDocs} from "firebase/firestore";
 import {db} from '../plugins/firebase';
 
 export default {
@@ -32,7 +33,7 @@ export default {
   methods: {
     async getCards() {
       const submittedCards = [];
-      const q = query(collection(db, 'rooms', 'dLgQh5FaRocuKWu0S9Ej', 'cards'), where("submitted", "==", true));
+      const q = query(collection(db, 'rooms', 'dLgQh5FaRocuKWu0S9Ej', 'cards'), where("submitted", "==", true), orderBy("id"));
       const querySnapshot = await getDocs(q);
       querySnapshot.forEach((doc) => {
         console.log(`${doc.id} => ${doc.data()}, ${doc.data().id}`);
